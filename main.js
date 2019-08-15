27344fcf0f400646ee28ebf4eab666f74ea58d3c
mitche50 <mitche50@gmail.com>
2019-08-14 22:43:24 -0400

    more logging

--- main.js
+++ main.js
@@ -8,10 +8,9 @@ var commands;
 async function parseMsg(msg) {
     //If the command exists, hand it off.
     if (typeof(commands[msg.text[0]]) !== "undefined") {
-        console.log("message being parsed: " + msg);
         console.log("msg.text: " + msg.text);
-        console.log("msg.channel: " + msg.channel);
-        console.log("msg.channel.type: " + msg.channel.type);
+        console.log("msg.channel: " + msg.obj.channel);
+        console.log("msg.channel.type: " + msg.obj.channel.type);
         await commands[msg.text[0]](msg);
         return;
     }
