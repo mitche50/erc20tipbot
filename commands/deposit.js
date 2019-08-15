bfa457a7f272cdefac4283840a45ed26cd3cf0cc
mitche50 <mitche50@gmail.com>
2019-08-15 13:34:57 -0400

    fix deposit address messaging

--- commands/deposit.js
+++ commands/deposit.js
@@ -3,9 +3,10 @@ module.exports = async (msg) => {
     if (msg.obj.channel.type != "dm") return;
     if (!(await process.core.users.getAddress(msg.sender))) {
         msg.obj.reply("You didn't have an address, generating one for you now ...")
-            .then(() => await process.core.users.setAddress(msg.sender, await process.core.coin.createAddress(msg.sender)))
+            .then(() => process.core.users.setAddress(msg.sender, await process.core.coin.createAddress(msg.sender)))
+            .then(() => msg.obj.reply("Your reusable address is " + await process.core.users.getAddress(msg.sender)))
             .catch(() => console.error("Error generating deposit address"));
     }
 
-    msg.obj.reply("Your reusable address is " + await process.core.users.getAddress(msg.sender));
+    ;
 };
