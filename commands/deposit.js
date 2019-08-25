850e7992ef890e737e209fc7a2861c5e21787fd1
mitche50 <mitche50@gmail.com>
2019-08-24 22:32:39 -0400

    bugfix deposit

--- commands/deposit.js
+++ commands/deposit.js
@@ -3,7 +3,7 @@ module.exports = async (msg) => {
     if (msg.obj.channel.type != "dm") return;
 
     if (!(await process.core.users.getAddress(msg.sender))) {
-        msg.obj.reply("You didn't have an address, generating one for you now ...")
+        notifyCreation()
             .then(() => process.core.coin.createAddress(msg.sender))
             .then((newAddress) => process.core.users.setAddress(msg.sender, newAddress))
             .then((newAddress) => sendAddress(newAddress))
@@ -19,5 +19,9 @@ module.exports = async (msg) => {
         } else {
             msg.obj.reply("There was an error generating your address.");
         }
+    }
+
+    async function notifyCreation() {
+        msg.obj.reply("You didn't have an address, generating one for you now ...");
     }
 };
